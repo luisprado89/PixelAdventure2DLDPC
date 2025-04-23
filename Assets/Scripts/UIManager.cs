b74672089765bf07489ca792bce8b44fef388286
luisprado89 <luisdpc89@gmail.com>
2025-04-24 01:19:24 +0200

    5 commit agregado menu
    Agregamos el menu para volver, salir, etc

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public void OptionsPanel()
+    {
+        Time.timeScale = 0;//Para parar el tiempo
+        optionsPanel.SetActive(true);
+
+    }
+    public void Return()
+    {
+        Time.timeScale = 1;//Para activar el tiempo
+        optionsPanel.SetActive(false);
+    }
+    public void AnotherOptions()
+    {
+        //Sound
+        //Graphics
+
+
+    }
+    public void GoMainMenu()
+    {
+        Time.timeScale = 1;//Para activar el tiempo
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {//Esto solo va a funcionar cuando este el ejecutable no dentro de unity
+        Application.Quit();
+    }
+
+
+}
